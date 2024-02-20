cbb362942a91d453fc997e658e7bf89f7165af57
Victor <frozen711.vr.vr@gmail.com>
2024-02-20 07:35:05 -0800

    moved the rest of the component CSS files out of the CSS folder

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import "@splidejs/splide/dist/css/splide.min.css";
 import { Link } from "react-router-dom";
-import "./css/Popular.css";
+import "./Popular.css";
 
 function Popular() {
   const [popular, setPopular] = useState([]);
